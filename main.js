d558d9b9bacabaa4d2f1ce010a9bdc134c495821
Javid Raina <rainajavid15@gmail.com>
2022-03-16 18:20:45 +0530

    Getting latitude and longitude on load

--- main.js
+++ main.js
@@ -1,230 +1,236 @@
-let input = document.querySelector('.search');
-let btn = document.querySelector('.fa-search');
-let locationName = document.querySelector('.locationName');
-let longLat = document.querySelector('.longLat');
-let temperature = document.querySelector('.temperature');
-let tempStatus = document.querySelector('.tempStatus');
-let feelsLike = document.querySelector('.feelsLike');
-let windSpeed = document.querySelector('.windSpeed');
-let humidity = document.querySelector('.humidity');
-let visibility = document.querySelector('.visibility');
-let weatherCondition = document.getElementById('weatherCondition');
-let sun = document.querySelector('.sun');
-let mainHeading = document.querySelector('.mainHeading');
-let mainPara = document.querySelector('.mainPara');
-let locationIcon = document.querySelector('.locationIcon');
-let rainDrop = weatherCondition.getElementsByClassName('rainDrop');
-let snowFlake = weatherCondition.getElementsByClassName('snowFlake');
-let emptyInput = document.querySelector('.emptyInput');
+let input = document.getElementById("search");
+let btn = document.querySelector(".fa-search");
+let weatherCondition = document.getElementById("weatherCondition");
+let sun = document.querySelector(".sun");
+let mainHeading = document.querySelector(".mainHeading");
+let mainPara = document.querySelector(".mainPara");
+let rainDrop = weatherCondition.getElementsByClassName("rainDrop");
+let snowFlake = weatherCondition.getElementsByClassName("snowFlake");
+let emptyInput = document.querySelector(".emptyInput");
+const weatherUpadateContainer = document.querySelector(
+  ".weatherUpadateContainer"
+);
+const visuals = document.getElementById("visuals");
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const API_KEY = "df401346306992b0bafdda5b176a1970";
 
+document.addEventListener("DOMContentLoaded", () => {
+  if (navigator)
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      fetchDataOnLoad(lat, lon);
+    });
+});
+
+const fetchDataOnLoad = async (lat, lon) => {
+  const API_ON_LOAD = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  try {
+    const response = await fetch(API_ON_LOAD);
+    if (!response.ok) throw new Error("city couldn't find");
+    const data = await response.json();
+    fetchData(data[0].name);
+  } catch (err) {
+    renderError(err);
+  }
+};
 // Fetching Data using fetch function
-const fetchData = () => {
-    if (input.value !== '') {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=df401346306992b0bafdda5b176a1970&units=metric`)
-            .then(res => res.json())
-            .then(data => {
-                displayData(data);
-            })
-            .catch(err => {
-                error(err);
-            });
+const fetchData = async (cityName) => {
+  if (cityName !== "") {
+    try {
+      const response = await fetch(
+        `${API_URL}${cityName}&appid=${API_KEY}&units=metric`
+      );
+      if (!response.ok) throw new Error("city couldn't find");
+      const data = await response.json();
+      displayData(data);
+    } catch (err) {
+      renderError(err);
     }
-}
+  }
+};
 //Defining data to be displayed on the basis of weather
 const displayData = (data) => {
-    //Setting background on the basis of weather conditon
-    if (data.weather[0].main == 'Rain') {
-        rain();
-    } else if (data.weather[0].main == 'Snow') {
-        snow()
-    } else if (data.weather[0].main == 'Clear') {
-        Clear()
-    } else if (data.weather[0].main == 'Smoke' || data.weather[0].main == 'Haze' || data.weather[0].main == 'Fog') {
-        smoke()
-    } else {
-        clouds()
-    }
-    //Displaying the data
-    locationIcon.style.display = 'inline-block';
-    locationName.innerHTML = data.name + ", " + data.sys.country;
-    longLat.innerHTML = `Lat: ${data.coord.lat} - Long: ${data.coord.lon}`
-    temperature.innerHTML = Math.trunc(data.main.temp) + "&deg;C";
-    tempStatus.innerHTML = `${data.weather[0].description}`;
-    feelsLike.innerHTML = `feels like: ${Math.trunc(data.main.feels_like)}&deg;C`;
-    windSpeed.innerHTML = `wind speed: ${data.wind.speed}mps`;
-    humidity.innerHTML = `humidity: ${data.main.humidity}%`;
-    visibility.innerHTML = `visibility: ${data.visibility / 1000}km`;
-}
+  let {
+    weather: [{ main }],
+  } = data;
+  //   main = "Rain";
+  renderVisuals(main);
+  renderWeatherDetails(data);
+};
+
+const renderVisuals = (mainWeather) => {
+  switch (mainWeather) {
+    case "Snow":
+      snow();
+      break;
+    case "Rain":
+      rain();
+      break;
+    case "Clear":
+      clear();
+      break;
+    case "Smoke":
+    case "Haze":
+    case "Fog":
+      smoke();
+      break;
+    default:
+      clouds();
+  }
+};
+
+const renderWeatherDetails = (data) => {
+  const {
+    name,
+    sys: { country },
+    coord: { lat, lon },
+    main: { temp, feels_like, humidity },
+    weather: [{ description }],
+    wind: { speed },
+    visibility,
+  } = data;
+  weatherUpadateContainer.innerHTML = `<div class="location">
+                <h1><i class="fas fa-street-view locationIcon"></i>
+                    <label class="locationName">${name}, ${country}</label>
+                </h1>
+                <p class="longLat">Lat: ${lat} - Long: ${lon}</p>
+            </div>
+            <div class="temp">
+                <h1 class="temperature">${Math.round(temp)}&deg;C</h1>
+                <p class="tempStatus">${description}</p>
+                <p class="feelsWind">
+                    <label class="feelsLike">feels like : ${Math.trunc(
+                      feels_like
+                    )}&deg;C</label>
+                    <label class="windSpeed">wind speed: ${speed}mps</label>
+                </p>
+                <p class="humvis">
+                    <label class="humidity">humidity: ${humidity}%</label>
+                    <label class="visibility">visibility: ${
+                      visibility / 1000
+                    }km</label>
+                </p>
+            </div>`;
+};
 
 //Defining Snow Condition
 const snow = () => {
-    weatherCondition.style.background = '#ccc';
-    mainHeading.style.display = 'none';
-    mainPara.style.display = 'none';
-    sun.style.display = 'none';
-    //hiding rainDrops
-    if (rainDrop.length !== 0) {
-        for (let i = 0; i <= rainDrop.length; i++) {
-            rainDrop[i].style.display = 'none';
-        }
+  visuals.innerHTML = null;
+  visuals.classList.remove("rain");
+  weatherCondition.style.background = "rgb(150, 150, 150)";
+  mainHeading.style.display = "none";
+  mainPara.style.display = "none";
+  sun.style.display = "none";
+  let cloud1 = document.querySelector(".cloud1");
+  cloud1.style.display = "none";
+  //Creating snowFlakes when they don't exist
+  if (snowFlake.length == 0) {
+    for (let i = 0; i <= 300; i++) {
+      let snowFlakes = document.createElement("i");
+      snowFlakes.className = "snowFlake";
+      let x = Math.random() * window.innerWidth;
+      let y = Math.floor(Math.random() * window.innerHeight - 200);
+      let height = Math.random() * 10;
+      let width = Math.random() * 10;
+      let duration = Math.floor(Math.random() * 7) + 2;
+      snowFlakes.style.height = height + "px";
+      snowFlakes.style.width = width + "px";
+      snowFlakes.style.top = y + "px";
+      snowFlakes.style.left = x + "px";
+      snowFlakes.style.animationDuration = duration + "s";
+      visuals.appendChild(snowFlakes);
     }
-    //Creating snowFlakes when they don't exist
-    if (snowFlake.length == 0) {
-        for (let i = 0; i <= 300; i++) {
-            let snowFlakes = document.createElement("i");
-            snowFlakes.className = "snowFlake";
-            let x = Math.random() * window.innerWidth;
-            let y = Math.random() * window.innerHeight;
-            let height = Math.random() * 10;
-            let width = Math.random() * 10;
-            let duration = Math.random() * 5;
-            snowFlakes.style.height = height + "px";
-            snowFlakes.style.width = width + "px";
-            snowFlakes.style.top = y + "px";
-            snowFlakes.style.left = x + "px";
-            snowFlakes.style.animationDuration = duration + "s";
-            weatherCondition.appendChild(snowFlakes);
-        }
-    }
-}
+  }
+};
 //Defining Rain Conditon
 const rain = () => {
-    weatherCondition.style.background = 'rgb(158, 196, 204)';
-    mainHeading.style.display = 'none';
-    mainPara.style.display = 'none';
-    sun.style.display = 'none';
-    //Hiding SnowFlakes
-    if (snowFlake.length !== 0) {
-        for (let i = 0; i <= snowFlake.length; i++) {
-            snowFlake[i].style.display = 'none';
-        }
-    }
-    //Creating Rain Drops when they don't exist
-    if (rainDrop.length == 0) {
-        for (let i = 0; i <= 300; i++) {
-            let rainDrops = document.createElement("i");
-            rainDrops.className = "rainDrop";
-            let x = Math.random() * window.innerWidth;
-            let y = Math.random() * window.innerHeight - 700;
-            let height = Math.random() * 50;
-            let width = Math.random() * 3;
-            let delay = Math.random() * 2;
-            rainDrops.style.height = height + "px";
-            rainDrops.style.width = width + "px";
-            rainDrops.style.top = y + "px";
-            rainDrops.style.left = x + "px";
-            rainDrops.style.animationDelay = delay + "s";
-            weatherCondition.appendChild(rainDrops);
-
-        }
-    }
-}
+  weatherCondition.style.background = "rgb(150, 150, 150)";
+  mainHeading.style.display = "none";
+  mainPara.style.display = "none";
+  sun.style.display = "none";
+  visuals.className = "rain";
+};
 
 //Defining Clouds Condition
 const clouds = () => {
-    //hiding rain drops and snowFlakes
-    if (rainDrop.length !== 0) {
-        for (let i = 0; i <= snowFlake.length; i++) {
-            drops[i].style.display = 'none';
-        }
+  //hiding rain drops and snowFlakes
+  if (snowFlake.length !== 0) {
+    for (let i = 0; i <= snowFlake.length; i++) {
+      snowFlake[i].style.display = "none";
     }
-    if (snowFlake.length !== 0) {
-        for (let i = 0; i <= snowFlake.length; i++) {
-            snowFlake[i].style.display = 'none';
-        }
-    }
-    //hiding the main heading and paragraph
-    mainHeading.style.display = 'none';
-    mainPara.style.display = 'none';
-    sun.style.display = 'block';
-    weatherCondition.style.background = '#77b5fe';
-}
+  }
+  //hiding the main heading and paragraph
+  mainHeading.style.display = "none";
+  mainPara.style.display = "none";
+  sun.style.display = "block";
+  weatherCondition.style.background = "#77b5fe";
+  visuals.innerHTML = null;
+  visuals.classList.remove("rain");
+};
 
 //Defining Smoke Condition
 const smoke = () => {
-    //hiding rain drops and snowFlakes
-    if (rainDrop.length !== 0) {
-        for (let i = 0; i <= snowFlake.length; i++) {
-            drops[i].style.display = 'none';
-        }
+  //hiding rain drops and snowFlakes
+  if (snowFlake.length !== 0) {
+    for (let i = 0; i <= snowFlake.length; i++) {
+      snowFlake[i].style.display = "none";
     }
-    if (snowFlake.length !== 0) {
-        for (let i = 0; i <= snowFlake.length; i++) {
-            snowFlake[i].style.display = 'none';
-        }
-    }
-    //hiding the main heading and paragraph
-    mainHeading.style.display = 'none';
-    mainPara.style.display = 'none';
-    sun.style.display = 'block';
-    weatherCondition.style.background = 'linear-gradient(#77b5fe,#ddd)';
-}
+  }
+  //hiding the main heading and paragraph
+  mainHeading.style.display = "none";
+  mainPara.style.display = "none";
+  sun.style.display = "block";
+  weatherCondition.style.background = "linear-gradient(#77b5fe,#ddd)";
+  visuals.innerHTML = null;
+  visuals.classList.remove("rain");
+};
 
 // Defining Clear Condition when the weather is Clear
-const Clear = () => {
-    //hiding rain drops and snowFlakes
-    if (rainDrop.length !== 0) {
-        for (let i = 0; i <= snowFlake.length; i++) {
-            drops[i].style.display = 'none';
-        }
+const clear = () => {
+  //hiding rain drops and snowFlakes
+  if (snowFlake.length !== 0) {
+    for (let i = 0; i <= snowFlake.length; i++) {
+      snowFlake[i].style.display = "none";
     }
-    if (snowFlake.length !== 0) {
-        for (let i = 0; i <= snowFlake.length; i++) {
-            snowFlake[i].style.display = 'none';
-        }
-    }
-    //hiding the main heading and paragraph
-    mainHeading.style.display = 'none';
-    mainPara.style.display = 'none';
-    sun.style.display = 'block';
-    weatherCondition.style.background = '#77b5fe';
-    let cloud1 = document.querySelector('.cloud1');
-    let cloud2 = document.querySelector('.cloud2');
-    let cloud3 = document.querySelector('.cloud3');
-    //cloud1.style.display = 'none';
-    cloud2.style.display = 'none';
-    cloud3.style.display = 'none';
-}
-const  error =(err) => {
-    mainHeading.style.display = 'block';
-    mainPara.style.display = 'block';
-    let drops = weatherCondition.getElementsByTagName('i').length;
-    /*if (drops !== undefined) {
-        for (let i = 0; i <= snowFlake.length; i++) {
-            drops[i].style.display = 'none';
-        }
-    }*/
-    locationIcon.style.display = '';
-    locationName.innerHTML = '';
-    longLat.innerHTML = ``;
-    temperature.innerHTML = '';
-    tempStatus.innerHTML = ``;
-    feelsLike.innerHTML = ``;
-    windSpeed.innerHTML = ``;
-    humidity.innerHTML = ``;
-    visibility.innerHTML = ``;
-    mainHeading.innerHTML = `<label style = "color: #f33">!City not found</label>`;
-    mainPara.innerHTML = `<label style = "color: #f33">Please Make sure you spelled correctly</label>`;
-}
+  }
+  //hiding the main heading and paragraph
+  mainHeading.style.display = "none";
+  mainPara.style.display = "none";
+  sun.style.display = "block";
+  weatherCondition.style.background = "#77b5fe";
+  // let cloud1 = document.querySelector('.cloud1');
+  let cloud2 = document.querySelector(".cloud2");
+  let cloud3 = document.querySelector(".cloud3");
+  // cloud1.style.display = 'none';
+  cloud2.style.display = "none";
+  cloud3.style.display = "none";
+  visuals.innerHTML = null;
+  visuals.classList.remove("rain");
+};
+const renderError = (err) => {
+  mainHeading.style.display = "block";
+  mainPara.style.display = "block";
+  weatherUpadateContainer.innerHTML = "";
+  mainHeading.innerHTML = `<label style = "color: #f33">Oops! city not found</label>`;
+  mainPara.innerHTML = `<label style = "color: #f33">Make sure city name is spelt correctly</label>`;
+};
 
 //loads data when the user press the enter key
-input.addEventListener('keyup', () => {
-    if(event.keyCode === 13){
-        if(input.value == ''){
-            emptyInput.className = "emptyInput showEmptyMessage";
-            setTimeout(() => {
-                emptyInput.className = "emptyInput";
-            },2000);
-        }else{fetchData();}
-        
-    }
+input.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") renderData(input.value);
 });
+
 // Loads data when the user clicks the search button
-btn.addEventListener('click', () => {
-    if(input.value == ''){
-        emptyInput.className = "emptyInput showEmptyMessage";
-        setTimeout(() => {
-            emptyInput.className = "emptyInput";
-        },2000);
-    }else{fetchData();}
+btn.addEventListener("click", () => {
+  renderData(input.value);
 });
+
+const renderData = (searchText) => {
+  if (!searchText) {
+    emptyInput.className = "emptyInput showEmptyMessage";
+    setTimeout(() => {
+      emptyInput.className = "emptyInput";
+    }, 2000);
+  } else fetchData(searchText);
+};
